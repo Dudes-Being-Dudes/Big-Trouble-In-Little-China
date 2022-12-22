f4d442f595cb7cf70a98ebf10258e4df3c2932fe
Camden Jenkins <camdenjenkins@yahoo.com>
2022-12-21 18:40:56 -0800

    made menu page like categories page

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -12,13 +12,14 @@ export default function Menu() {
   return (
     <>
       {<MenuDropdown />}
-      <div>
-        {" "}
-        All Items
+      <div class="grid lg:grid-cols-4 gap-6 md:grid-cols-2  flex-wrap justify-around ">
         {items.map((item) => {
           return (
-            <div>
+            <div class="rounded-lg border-solid border-2 border-slate-700 flex flex-col h-auto w-auto gap-y-6 ml-2 items-center">
               <h3>{item.name}</h3>
+              <p>{item.description}</p>
+              <img className="h-40 mt-0" src={item.imageUrl} />
+              <p>${item.price}</p>
             </div>
           );
         })}
