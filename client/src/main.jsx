4fdb8cd1e5fe2bd69e7b3b9d4cb017ee22732ddc
Carson Craft <ccraft4444@gmail.com>
2022-12-21 16:57:06 -0800

    added menu component, navbar, cat dropdown

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import store from "./store";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <StoreProvider store={store}>
-      <App />
-    </StoreProvider>
+    <BrowserRouter>
+      <StoreProvider store={store}>
+        <App />
+      </StoreProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
