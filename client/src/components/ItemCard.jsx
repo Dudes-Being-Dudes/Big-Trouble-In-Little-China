83b745b09a5343a51071235138556db13d464603
Wilsonrchen <Wilsonrchen@gmail.com>
2023-03-04 21:39:03 -0800

    fixed some seeddata

--- client/src/components/ItemCard.jsx
+++ client/src/components/ItemCard.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLeaf,
+  faPepperHot,
+  faSeedling,
+} from "@fortawesome/free-solid-svg-icons";
 
 const ItemCard = ({ item }) => {
   console.log(item);
@@ -8,6 +14,34 @@ const ItemCard = ({ item }) => {
       className=" flex flex-col shadow-md h-auto w-auto gap-y-6 ml-2 my-2 items-center"
     >
       <img src={item.imageUrl} alt="No Picture Available" />
+      <div className="flex items-center gap-2">
+        {item.isSpicy === true ? (
+          <FontAwesomeIcon
+            icon={faPepperHot}
+            className="text-red-700 px-2 py-2 rounded-full"
+            title="spicy"
+          />
+        ) : null}
+        {item.isGlutenFree === true ? (
+          <span class="px-1 py-1 rounded-full bg-gray-100" title="gluten free">
+            GF
+          </span>
+        ) : null}
+        {item.isVegan === true ? (
+          <FontAwesomeIcon
+            icon={faSeedling}
+            className="px-2 py-2 rounded-full bg-green-100 text-emerald-600"
+            title="vegan"
+          />
+        ) : null}
+        {item.isVegetarian === true ? (
+          <FontAwesomeIcon
+            icon={faLeaf}
+            className="px-2 py-2 rounded-full  text-green-400"
+            title="vegetarian"
+          />
+        ) : null}
+      </div>
       <h3 className="text-phoRed">{item.name}</h3>
       <p className="mx-4">{item.description}</p>
       <p>${item.price}</p>
