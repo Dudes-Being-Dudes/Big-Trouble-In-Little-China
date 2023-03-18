c7a9bfa692a2a2802812a7bc1f137d9e33f78ef1
Wilsonrchen <Wilsonrchen@gmail.com>
2023-03-17 19:29:43 -0700

    added covid policy

--- client/src/components/Covid19Policy.jsx
+++ client/src/components/Covid19Policy.jsx
@@ -7,7 +7,31 @@ const Covid19Policy = () => {
         <h1 className={styles.heading}>Covid-19 Policy</h1>
       </div>
       <div className={styles.body}>
-        <div className="p-4 bg-snow m-10 rounded text-black"></div>
+        <div className="p-4 bg-snow m-10 rounded text-black">
+          <p>
+            Just a quick note to reassure you that we are doing all we can to
+            keep you and our staff safe. We are following government guidelines,
+            implementing key safety practices following our detailed risk
+            assessment of our processes & will continue to adapt these as
+            necessary.
+          </p>
+          <p>We are:</p>
+          <ul className="list-disc">
+            <li>checking our staff are well with daily health declarations</li>
+            <li>increasing hand-washing</li>
+            <li>regularly sanitising surfaces</li>
+            <li>wearing masks where necessary</li>
+          </ul>
+          <p>
+            We ask that all customers only visit the restaurant if they are
+            feeling well, haven't had any symptoms and to their knowledge have
+            not been in contact with anyone who has symptoms.
+          </p>
+        </div>{" "}
+        <div className="justify-center items-center flex flex-col">
+          <p>We hope to see you soon,</p>
+          <p>China Center Restaurant</p>
+        </div>
       </div>
     </>
   );
